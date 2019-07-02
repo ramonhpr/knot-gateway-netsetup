3573cf9920f57195abda4dc0bd22e4436d7c3939
Ramon Ribeiro <rhpr@cesar.org.br>
2019-07-01 22:56:09 -0300

    connman: Enable tether when starting daemon
    This patch enable tethering when daemon starts. It allow connectivity
    to gateway even when it has no network nearby.
    
    Signed-off-by: Ramon Ribeiro <rhpr@cesar.org.br>

---
+++ netsetup/connman.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, CESAR. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import dbus
+from dbus.exceptions import DBusException
+
+CONNMAN_SERVICE_NAME = 'net.connman'
+CONNMAN_MANAGER_INTERFACE = '%s.Manager' % CONNMAN_SERVICE_NAME
+CONNMAN_TECHNOLOGY_INTERFACE = '%s.Technology' % CONNMAN_SERVICE_NAME
+
+DEFAULT_SSID = 'knot_gw'
+DEFAULT_PSW = 'knotNetworkOfThings'
+
+
+class ConnmanClient(object):
+    def __get_manager_interface(self):
+        try:
+            return dbus.Interface(
+                self.bus.get_object(CONNMAN_SERVICE_NAME, '/'),
+                CONNMAN_MANAGER_INTERFACE)
+        except DBusException as err:  # ignore if connmand is not running
+            logging.error('%s: Connman is probably not running.' % err)
+            return
+
+    def __init__(self):
+        self.bus = dbus.SystemBus()
+
+        self.manager = self.__get_manager_interface()
+
+    def __enable_wifi(self, iface):
+        properties = iface.GetProperties()
+        if not properties.get('Powered'):
+            iface.SetProperty('Powered', dbus.Boolean(True))
+            logging.info('Wifi enabled')
+
+    def __get_wifi_technology_path(self):
+        if not self.manager:
+            logging.error('Unable to get wifi technology. Connman not running')
+            return
+
+        for obj_path, properties in self.manager.GetTechnologies():
+                if properties.get('Type') == 'wifi':
+                        return obj_path
+
+    def enable_tethering(self):
+        path = self.__get_wifi_technology_path()
+
+        if not path:
+            logging.error('Not found technology wifi')
+            return
+
+        wifi_tech = dbus.Interface(
+            self.bus.get_object(CONNMAN_SERVICE_NAME, path),
+            CONNMAN_TECHNOLOGY_INTERFACE)
+
+        self.__enable_wifi(wifi_tech)
+        try:
+                wifi_tech.SetProperty('TetheringIdentifier',
+                                      dbus.String(DEFAULT_SSID))
+                wifi_tech.SetProperty('TetheringPassphrase',
+                                      dbus.String(DEFAULT_PSW))
+                wifi_tech.SetProperty('Tethering', dbus.Boolean(True))
+                logging.info('Tethering enabled: SSID %s PSW: %s',
+                             DEFAULT_SSID, DEFAULT_PSW)
+        except DBusException as err:
+                logging.error('DBus error')
+                logging.error(err)
