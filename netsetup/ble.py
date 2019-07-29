c6843638fe1f5b74d5c52bfbdd2e44db1f0fc011
Ramon Ribeiro <rhpr@cesar.org.br>
2019-07-28 22:12:03 -0300

    ble: gatt: wifi: Connect when characteristics set
    This patch makes a connection with the wifi network set by the
    characteristics SSID and Password in the GATT service be set.
    
    Signed-off-by: Ramon Ribeiro <rhpr@cesar.org.br>

--- netsetup/ble.py
+++ netsetup/ble.py
@@ -25,17 +25,14 @@ from ble_util.constants.dbus_interfaces import GATT_MANAGER_IFACE
 logging.basicConfig(format='[%(levelname)s] %(funcName)s: %(message)s\n',
                     stream=sys.stderr, level=logging.INFO)
 
-wpantun = None
-
 ADVERTISEMENT_TOOGLE_MS = 500
 
 
 class KnotApplication(Application):
-    def __init__(self, bus):
-        global wpantun
+    def __init__(self, bus, wpantun, connman):
         Application.__init__(self, bus)
         self.add_service(OpenthreadService(bus, 0, wpantun))
-        self.add_service(WifiService(bus, 1))
+        self.add_service(WifiService(bus, 1, connman))
 
 
 class KnotAdvertisement(Advertisement):
@@ -67,11 +64,9 @@ class BleService(object):
     gatt_manager = None
     gatt_knot = None
 
-    def __init__(self, wpan, ad_name):
-        global wpantun
+    def __init__(self, wpantun, connman, ad_name):
 
         self.bus = dbus.SystemBus()
-        wpantun = wpan
 
         self.ad_adapter = find_adapter(self.bus, LE_ADVERTISING_MANAGER_IFACE)
         if not self.ad_adapter:
@@ -89,7 +84,7 @@ class BleService(object):
                                 self.ad_adapter),
             LE_ADVERTISING_MANAGER_IFACE)
 
-        self.gatt_knot = KnotApplication(self.bus)
+        self.gatt_knot = KnotApplication(self.bus, wpantun, connman)
 
         self.ad_knot = KnotAdvertisement(self.bus, 0, self.gatt_knot, ad_name)
 
